71f929d559107a1ea55a7f449fb51eadde5da1a5
diwei <2623556566@qq.com>
2024-07-31 13:16:58 +0800

    asd

--- include/interface_ros1.h
+++ include/interface_ros1.h
@@ -1,10 +1,7 @@
 #pragma once
-#include "pgo/Backend.hpp"
-
-void load_ros_parameters(bool &path_en, bool &scan_pub_en, bool &dense_pub_en,
-                         std::string &gnss_topic, std::string &map_frame, std::string &lidar_frame);
-void load_parameters(Backend &backend);
-void load_pgm_parameters(bool &save_globalmap_en, bool &save_pgm, double &pgm_resolution, float &min_z, float &max_z);
+#include <ros/ros.h>
+#include "Header.h"
 
 void init_pgo_system(ros::NodeHandle &nh);
+
 void pgo_handle(PointXYZIRPYT &this_pose6d, PointCloudType::Ptr &feats_undistort, PointCloudType::Ptr &submap_fix);
--- include/interface_ros2.h
+++ include/interface_ros2.h
@@ -1,14 +1,7 @@
 #pragma once
 #include <rclcpp/rclcpp.hpp>
-#include "pgo/Backend.hpp"
-
-void load_ros_parameters(rclcpp::Node::SharedPtr &node, bool &path_en, bool &scan_pub_en, bool &dense_pub_en,
-                         std::string &gnss_topic, std::string &map_frame, std::string &lidar_frame);
-
-void load_parameters(rclcpp::Node::SharedPtr &node, Backend &backend);
-
-void load_pgm_parameters(rclcpp::Node::SharedPtr &node, bool &save_globalmap_en, bool &save_pgm, double &pgm_resolution, float &min_z, float &max_z);
-
-void pgo_handle(PointXYZIRPYT &this_pose6d, PointCloudType::Ptr &feats_undistort, PointCloudType::Ptr &submap_fix);
+#include "Header.h"
 
 void init_pgo_system(rclcpp::Node::SharedPtr &node);
+
+void pgo_handle(PointXYZIRPYT &this_pose6d, PointCloudType::Ptr &feats_undistort, PointCloudType::Ptr &submap_fix);
